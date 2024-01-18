a5a9cee7d939522f4cdc6dc9507793b53fa4e7f0
velmurugan3 <velmurugan@saasforest.com>
2024-01-18 12:58:48 +0530

    Updated

--- resources/views/tables/columns/employee-profile.blade.php
+++ resources/views/tables/columns/employee-profile.blade.php
@@ -1,15 +1,10 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <div class=" w-full">
-    {{-- {{ $getState() }} --}}
-    {{-- <h1>Velu</h1> --}}
-  
-    {{-- bal;achandran --}}
     <div class=" flex justify-end">
         <button class=" text-sm text-[#287D3C] border border-[#287D3C] rounded px-1">Active</button>
     </div>
-    {{-- <div class="flex justify-end"><p class="ml-3">active</p></div> --}}
     <div class=" flex justify-center">
-        <img src="/images/profile.png" alt="">
+        <img src="/images/profile.png" alt="" class=" w-[85px] h-[85px]">
     </div>
     <div class=" text-center mt-2">
         <div><span class=" text-[#6B7280] font-medium">EMP001</span> <span class=" font-medium">-   {{$getRecord()->name }}</span></div>
